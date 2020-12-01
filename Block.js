795eceba683b49a57a62ee7f197c164e268bf254
ujasbatheja <71878150+ujasbatheja@users.noreply.github.com>
2020-12-02 01:16:31 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,41 @@
+class Block {
+    constructor(x,y,width,height) {
+      var options = {
+          //isStatic: false,
+          'restitution':0.4,
+          'friction':0.0
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      this.Visibility = 255;
+    }
+    display(){
+      console.log(this.body.speed);
+      if(this.body.speed<3){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x,pos.y);
+      angleMode(RADIANS);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("red");
+      rect(0,0, this.width, this.height);
+      pop();
+      }
+      else{
+        World.remove(world,this.body);
+         push();
+         this.visibility = this.visibility-5;
+         tint(255,this.Visibility);
+         pop();
+      }
+    }
+    blockScore(){
+      if(this.Visiblity===0){
+        score=score+5; 
+      }
+    }
+  }
